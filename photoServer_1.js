69580d7e203f05c94846bc55d2764fc9a99ed896
pvegliante@fvi.edu <pvegliante@fvi.edu>
2017-04-26 22:25:37 -0400

    fix bug

--- photoServer_1.js
+++ photoServer_1.js
@@ -3,38 +3,53 @@ var fs = require('fs');
 const PORT = 8080;
 
 function load_album_list(callback) {
-  // we assume that any directory inside albums is an "album"
-  fs.readdir(
-    'albums',
-    function(err, files) {
-      if(err) {
-        callback(err);
-        return;
-      }
-      callback(null, files);
-    }
-  );
+    // we assume that any directory inside albums is an "album"
+    fs.readdir(
+        'albums',
+        function(err, files) {
+            if (err) {
+                callback(err);
+                return;
+            }
+
+            var only_dirs = [];
+
+            for (let i = 0; files && i < files.length; i++) {
+                fs.stat('albums/' + files[i], (err, stats) => {
+                    if (stats.isDirectory()) {
+                        only_dirs.push(files[i]);
+                    }
+                });
+            }
+
+            callback(null, only_dirs);
+    });
 }
 
 function handle_incoming_request(req, res) {
-  console.log('INCOMING REQUEST: ' + req.method+ ' ' + req.url);
-  load_album_list(function(err, albums) {
-    if(err) {
-      res.writeHead(500, {"Content-Type": "application/json"});
-    res.end(JSON.stringify(err) + "\n");
-    return;
-    }
+    console.log('INCOMING REQUEST: ' + req.method + ' ' + req.url);
+    load_album_list(function(err, albums) {
+        if (err) {
+            res.writeHead(500, {
+                "Content-Type": "application/json"
+            });
+            res.end(JSON.stringify(err) + "\n");
+            return;
+        }
 
-    var out = { error: null, data: {albums: albums }};
-    res.writeHead(200, {"Content-Type": "application/json"})
-    res.end(JSON.stringify(out) + "\n");
-  });
+        var out = {
+            error: null,
+            data: {albums: albums}
+        };
+        res.writeHead(200, {
+            "Content-Type": "application/json"
+        })
+        res.end(JSON.stringify(out) + "\n");
+    });
 }
 
 var server = http.createServer(handle_incoming_request);
 
-
-
 server.listen(PORT, () => {
-  console.log('listening on http://localhost: ' + PORT);
+    console.log('listening on http://localhost: ' + PORT);
 });
