18914e7f2878129f9c6f881a5c39898789613f83
pvegliante@fvi.edu <pvegliante@fvi.edu>
2017-05-09 22:03:14 -0400

    throw er

---
+++ auth-server/index.js
@@ -0,0 +1,20 @@
+const mongoose = require('mongoose');
+
+const User = require('./user');
+
+mongoose.connect('mongodb://pete:kissmyass2006@ds133311.mlab.com:33311/newlion', (err) => {
+  // Create a new document using our model
+  const me = new User({
+  firstName: 'Pete',
+  lastName: 'Vegliante',
+  userName: 'pveg',
+  email: 'pvegliante@fvi.edu',
+  password: 'password'
+  });
+
+  me.save((err) => {
+    if (err) {
+      throw err;
+    }
+  });
+});
---
+++ auth-server/user.js
@@ -0,0 +1,87 @@
+const mongoose = require('mongoose');
+const bcrypt = require('bcryptjs');
+
+const SALT_WORK_FACTOR = 10;
+
+let User;
+
+const UserSchema = new mongoose.Schema({
+  firstName: { type: String, require: true},
+  lastName: { type: String, },
+  userName: { type: String },
+  email: {
+    type: String,
+    require: true,
+    index: { unique: true }
+  },
+  password: { type: String, require: true }
+});
+
+//validation
+//------------------------
+
+UserSchema.path('email').validate((email) => {
+  return /.?@.?\./.test(email);
+}, 'Invalid email address');
+
+// filter
+//
+UserSchema.pre('save', function(next) {
+  const user = this;
+
+  if (!user.isModified('password')) return next();
+
+  bcrypt.genSalt(SALT_WORK_FACTOR, function(err, salt) {
+    if (err) return next(err);
+
+    bcrypt.hash(user.password, salt, function(err, hash) {
+
+      user.password = hash;
+      next();
+    });
+  });
+});
+
+// static methods
+// -------------
+UserSchema.static('attemptLogin', (email, password, cb) => {
+  User.findByEmail(email, (err, user) => {
+    if (err) { return cb(err); }
+
+    if (!user) {
+      return cb();
+    }
+
+    user.comparePassword(password, (err, isMatch) => {
+      if (err)  { return cb(err); }
+
+      if(isMatch) {
+        return cb(null, user);
+      } else {
+        return cd();
+      }
+    });
+  });
+});
+
+UserSchema.methods.comparePassword = function(candidatePassword, cd) {
+  bcrypt.compare(candidatePassword, this.password, (err,isMatch) => {
+    if (err) { throw err; }
+    cb(null, isMatch);
+  });
+};
+
+// virtual fields
+//--------------
+
+UserSchema.virtual('fullName').get(function() {
+  return `${this.firstName} ${this.lastName}`;
+});
+
+UserSchema.static('findByEmail', (email, cb) => {
+  User.findOne({ email: email }, cb);
+});
+
+User = mongoose.model('user', UserSchema);
+
+module.exports = User;
